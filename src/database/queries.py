2460ea0bf1e86804fa377ebfc98ae9b1cdb9eb02
Nixuge <mail@nixuge.me>
2023-05-05 14:11:02 +0200

    Moved src files over to src/ folder

--- src/database/queries.py
+++ src/database/queries.py
@@ -66,3 +66,51 @@ class Queries:
     @staticmethod
     def get_where_contains_md5(table: str, hash: str):
         return Queries.get_where_contains_key(table, "md5sum", hash)
+
+class QueriesChomikuj:
+    @staticmethod
+    def get_create_repo_table_query(name: str) -> str:
+        return f"""CREATE TABLE IF NOT EXISTS {name} (
+                    package VARCHAR(255) NOT NULL,
+                    name VARCHAR(255),
+                    version VARCHAR(255),
+
+                    section VARCHAR(255),
+                    architecture VARCHAR(255),
+                    priority VARCHAR(255),
+                    tag VARCHAR(2048),
+
+                    depends VARCHAR(2048),
+                    provides VARCHAR(2048),
+                    predepends VARCHAR(2048),
+                    replaces VARCHAR(2048),
+                    suggests VARCHAR(2048),
+                    breaks VARCHAR(2048),
+                    conflicts VARCHAR(2048),
+
+                    size INT,
+                    installedsize INT,
+
+                    author VARCHAR(1024),
+                    maintainer VARCHAR(1024),
+                    support VARCHAR(1024),
+                    dev VARCHAR(1024),
+
+                    homepage VARCHAR(1024),
+                    description TEXT,
+
+                    depiction VARCHAR(32),
+                    moderndepiction VARCHAR(32),
+
+                    icon VARCHAR(32),
+                    header VARCHAR(32),
+
+                    additionaldata TEXT,
+                    
+                    md5sum VARCHAR(32) NOT NULL,
+                    sha256 VARCHAR(64),
+
+                    paid BOOLEAN,
+
+                    PRIMARY KEY (md5sum)
+                    );"""
